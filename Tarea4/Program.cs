258a7f758d11d61989697f46d2417016988afe0b
Bryan <bryanjosueriverahernandez@gmail.com>
2024-02-24 11:33:00 -0600

    Agregar archivos de proyecto.

---
+++ Tarea4/Program.cs
@@ -0,0 +1,35 @@
+﻿//int x = 2;
+//while (x <= 10)
+//{
+//    Console.WriteLine(x);
+//    x = x + 2;
+//}
+
+//int x = 1;
+//int suma = 0;
+//while (x <= 10)
+//{
+//    suma += x;
+//    Console.WriteLine(x);
+//    x++;
+//}
+//Console.WriteLine(suma);
+//int num = 0;
+//do
+//{
+//    Console.WriteLine("ingrese un numero entero");
+//    int.TryParse(Console.ReadLine(), out num);
+//}
+//while (num >= 0);
+//{
+//    Console.WriteLine("ingreso un numero negativo el programa se cerrara");
+int numero = 1;
+while (true)
+{
+
+    if (numero % 7 == 0 && numero % 11 == 0)
+    {
+        Console.WriteLine("¡El número es divisible entre 7 y 11! Cerrando el programa.");
+        break;
+    }
+}
